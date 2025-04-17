2200815aaf28893ecf44a663cebd9f1d4a7db6ea
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-04-17 17:51:23 +0300

    feat(auth): create basic app structure

---
+++ AekzamiTest/AekzamiTest/App/AekzamiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AekzamiTestApp.swift
+//  AekzamiTest
+//
+//  Created by Kirill Sklyarov on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AekzamiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
